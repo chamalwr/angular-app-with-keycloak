c5396173eaf4c693b57c7a72d731b3073bcbf9bf
Chamal Weerasinghe <chamalwr@users.noreply.github.com>
2021-06-25 17:52:42 +0530

    [fix]Configurations modification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from './auth/keycloak.guard';
 
 const routes: Routes = [
   { path: 'pet', loadChildren: ()=> import('./pet/pet.module').then(m => m.PetModule), canActivate: [AuthGuard] },
-  { path: 'admin', loadChildren: ()=> import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'admin', loadChildren: ()=> import('./admin/admin.module').then(m => m.AdminModule), canActivate: [AuthGuard] },
   { path: 'owner', loadChildren: () => import('./owner/owner.module').then(m => m.OwnerModule), canActivate: [AuthGuard]  }
 ];
 
