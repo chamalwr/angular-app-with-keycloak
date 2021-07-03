d8a88ca635033e15cd3391209f12f8aaac5baa2d
Chamal Weerasinghe <chamalwr@users.noreply.github.com>
2021-07-03 20:37:35 +0530

    [fix]Routes and Modules

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponentComponent } from '../common/page-not-found-component/page-not-found-component.component';
 import { AddAdminComponent } from './component/add-admin/add-admin.component';
 import { EditAdminComponent } from './component/edit-admin/edit-admin.component';
 import { ListAdminComponent } from './component/list-admin/list-admin.component';
+import {PageNotFoundComponent} from '../ppc-commons/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'admins', pathMatch: 'full' },
   { path: 'admins', component: ListAdminComponent },
   { path: 'addadmin', component: AddAdminComponent },
   { path: 'editadmin', component: EditAdminComponent },
-  { path: '**', component: PageNotFoundComponentComponent }
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/owner/owner-routing.module.ts
+++ src/app/owner/owner-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponentComponent } from '../common/page-not-found-component/page-not-found-component.component';
+import { PageNotFoundComponent } from '../ppc-commons/page-not-found/page-not-found.component';
 import { AddOwnerComponent } from './component/add-owner/add-owner.component';
 import { EditOwnerComponent } from './component/edit-owner/edit-owner.component';
 import { ListOwnerComponent } from './component/list-owner/list-owner.component';
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: 'owners', component: ListOwnerComponent },
   { path: 'addOwner', component: AddOwnerComponent},
   { path: 'editOwner', component: EditOwnerComponent},
-  { path: '**', component: PageNotFoundComponentComponent}
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
